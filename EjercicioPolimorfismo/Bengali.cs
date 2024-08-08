a3f89e90808460ea78455918050bc414e5532620
PaoDerass <paola_deras@unitec.edu>
2024-08-08 15:27:54 -0600

    Creacion de clases y metodos del gestor de gatos

---
+++ EjercicioPolimorfismo/Bengali.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace EjercicioPolimorfismo
+{
+    public class Bengali : Gato
+    {
+        public override void Sonido()
+        {
+            Console.WriteLine($"{Nombre} Maulla Fuerte, como un rugido");
+        }
+        public override void Informacion()
+        {
+            Console.WriteLine($"{Nombre} es un gato Maine Coon Tiene sus orígenes en Estados Unidos y es muy popular en Europa y América del Norte. ¿Sabías que sus raíces guardan algunas curiosidades? Su nombre “Maine” deriva del estado de Maine de EEUU y “Coon” proviene de “racoon” que significa mapache. Y es que se dice que esta raza de felino es una mezcla entre los mapaches y los gatos salvajes.");
+        }
+    }
+}
